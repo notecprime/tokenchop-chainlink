c5fbc97d047d9664aad4e6aa2a00bdb39c1a843c
codebrent <codebrent@users.noreply.github.com>
2021-03-20 11:20:54 +1300

    Unboxed chainlink truffle starter

---
+++ truffle-config.js
@@ -0,0 +1,32 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+require('dotenv').config();
+
+const mnemonic = process.env.MNEMONIC
+const url = process.env.RPC_URL
+
+module.exports = {
+  networks: {
+    cldev: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '*',
+    },
+    ganache: {
+      host: '127.0.0.1',
+      port: 7545,
+      network_id: '*',
+    },
+    kovan: {
+      provider: () => {
+        return new HDWalletProvider(mnemonic, url)
+      },
+      network_id: '42',
+      skipDryRun: true
+    },
+  },
+  compilers: {
+    solc: {
+      version: '0.6.12',
+    },
+  },
+}
